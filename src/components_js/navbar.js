2ab215f5e92e765a90ea5e516fd4ac3745072688
xCrowly <ahhhhhmedeeeee@gmail.com>
2022-08-05 16:50:51 +0200

    added the navbar

---
+++ src/components_js/navbar.js
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-md fixed-top 
+        bg-light shadow w-100 bg-opacity-75">
+      <div className="container-fluid">
+        <div className="mx-1 text-info">
+          <FontAwesomeIcon icon={faCode} />
+        </div>
+        <a className="navbar-brand fw-bolder text-primary nav-item-name" href="#img-header">Ahmed Badawy</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#main-sec">About me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects-sec">My projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#skills-sec">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#footer-sec">Contact me</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/scss/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components_js/navbar';
+import BootStrap from 'bootstrap';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const navbar = ReactDOM.createRoot(document.getElementById('navbar'));
+navbar.render(
+    <Navbar />
 );
 
 // If you want to start measuring performance in your app, pass a function
